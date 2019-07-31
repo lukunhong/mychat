24728774cf592094dfaddbe6fff55791d419705d
kuhn554 <30819241+kuhn554@users.noreply.github.com>
2019-07-31 13:30:43 +0800

    点对点发送

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -6,6 +6,10 @@ class Index extends Controller
 {
     public function index()
     {
+        $from_id = input('from_id');
+        $to_id = input('to_id');
+        $this->assign('from_id',$from_id);
+        $this->assign('to_id',$to_id);
         return $this->fetch();
     }
 }
